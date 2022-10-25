1b04bb43d6f458a31209ad6375070a284260089e
rohitgulam <rohitsgulam@gmail.com>
2022-10-25 17:45:44 +0300

    created dashboard view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<p>
+
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus ipsa asperiores consectetur et dolorem porro earum maiores quaerat ab enim. lorem600
+</p>
+    
+@endsection
