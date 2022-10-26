ee159d81757b9b01b876e23c9aecf34515b7b1dc
rohitgulam <rohitsgulam@gmail.com>
2022-10-26 14:36:44 +0300

    :sparkles: added action buttons

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,34 +4,54 @@
 
     <div class="pt-24">
         <div class="w-11/12 flex flex-col items-center justify-center">
-            <h2 class="text-3xl py-6" >Orodha ya bizaa zote</h2>
+            <h2 class="text-3xl py-6" >Orodha ya bidhaa zote</h2>
             <table class="table-auto text-left border border-collapse border-gray-400">
                 <thead>
                     <tr>
-                        <th class="pr-12 border border-gray-400 py-2" >Jina la bidhaa</th>
-                        <th class="pr-12 border border-gray-400 py-2">Kategoria</th>
-                        <th class="pr-12 border border-gray-400 py-2">Idadi iliyopo store</th>
-                        <th class="pr-12 border border-gray-400 py-2" >Maelezo</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2 text-center" >#</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2" >Jina la bidhaa</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2">Kategoria</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2">Idadi iliyopo store</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2" >Maelezo</th>
+                        <th class="pr-12 pl-4 border border-gray-400 py-2 text-center" >Vitendo</th>
                     </tr>
                 </thead>
                 <tbody class="text-gray-500">
                     @foreach ($products as $product)
                         <tr>
-                            <td class="pr-12 border border-gray-400 py-2" > {{$product->name}}</td>
-                            <td class="pr-12 border border-gray-400 py-2"> 
+                            <td class="pr-12 pl-4 border border-gray-400 py-2 text-center" > {{$loop->iteration}}</td>
+                            <td class="pr-12 pl-4 border border-gray-400 py-2" > {{$product->name}}</td>
+                            <td class="pr-12 pl-4 border border-gray-400 py-2"> 
                                 @if ($product->category == null)
                                     Haina Kategoria
                                 @else
                                 {{$product->category}}
                                 @endif 
                             </td>
-                            <td class="pr-12 border border-gray-400 py-2 text-center"> {{$product->quantity}} </td>
-                            <td class="pr-12 border border-gray-400 py-2" > 
+                            <td class="pr-12 pl-4 border border-gray-400 py-2 text-center"> {{$product->quantity}} </td>
+                            <td class="pr-12 pl-4 border border-gray-400 py-2" > 
                                 @if ($product->description == null)
                                     Hamna maelezo
                                 @else 
                                 {{$product->description}}
                                 @endif 
+                            </td>
+                            <td class="px-6 border border-gray-400 py-1">  
+                                <div class="flex">
+                                    <form action="/products/{{$product->id}}/edit" method="GET">
+                                        @csrf
+                                        <button class="bg-blue-600 text-white rounded py-2 px-4 mx-2 hover:bg-blue-700">
+                                            Hariri
+                                        </button>
+                                    </form>
+                                    <form action="/products/{{$product->id}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="bg-red-500 text-white rounded py-2 px-4 mx-2 hover:bg-red-600">
+                                            Futa
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
