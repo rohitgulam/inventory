6995b3d6b9d93ad4b82c28392173fdb128f33f14
rohitgulam <rohitsgulam@gmail.com>
2022-10-27 14:05:26 +0300

    :sparkles: crated purchase controller

---
+++ app/Models/Purchase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'product',
+        'quantity',
+        'price',
+        'credit',
+        'creditor',
+        'purchase_by',
+        'purchase_from',
+        'product_id'
+    ];
+}
