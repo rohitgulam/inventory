5b2687963ebe8a07497e0d76b8951ccb0e8c445b
rohitgulam <rohitsgulam@gmail.com>
2022-10-31 12:04:42 +0300

    :sparkles: added new fields

--- database/migrations/2022_10_24_124504_create_products_table.php
+++ database/migrations/2022_10_24_124504_create_products_table.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('description')->nullable();
             $table->string('category')->nullable();
+            $table->string('quality');
+            $table->string('metric');
+            $table->string('selling_price');
+            $table->integer('bonus')->default(0);
             $table->integer('quantity')->default(0);
             $table->integer('deleted')->default(0);
             $table->timestamps();
