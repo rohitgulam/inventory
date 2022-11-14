ec0460067b2c30311eb883da2b3aaf7f1bfae06c
rohitgulam <rohitsgulam@gmail.com>
2022-11-14 10:56:59 +0300

    :lipstick: added description column

--- resources/views/purchases.blade.php
+++ resources/views/purchases.blade.php
@@ -16,10 +16,10 @@
                 <tr>
                     <th class="px-6 border border-gray-400 py-2 text-center" >#</th>
                     <th class="px-6 border border-gray-400 py-2" >Jina la bidhaa</th>
-                    {{-- <th class="px-6 border border-gray-400 py-2">Kategoria</th> --}}
                     <th class="px-6 border border-gray-400 py-2">Idadi iliyonunuliwa</th>
                     <th class="px-6 border border-gray-400 py-2" >Bei ya moja</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Bei ya jumla</th>
+                    <th class="px-6 border border-gray-400 py-2">Description</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Imenunuliwa kwa mkopo</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Imenunuliwa kutoka kwa nani</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Nani aliyefanya manunuzi</th>
@@ -34,6 +34,7 @@
                         <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$purchase->quantity}} </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$purchase->price}} </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$purchase->unit_sum}} </td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$purchase->description}} </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2 text-center">
                             @if ($purchase->credit == 1)
                             Mkopo
