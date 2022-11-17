1ca55476d94e88bd6a6c5ae496d6060264dd3a64
rohitgulam <rohitsgulam@gmail.com>
2022-11-17 15:25:12 +0300

    :sparkles: created Sell model

---
+++ app/Models/Sell.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sell extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product',
+        'quantity',
+        'price',
+        'unit_sum',
+        'credit',
+        'creditor',
+        'sell_by',
+        'sell_to',
+        'product_id',
+        'description'
+    ];
+}
