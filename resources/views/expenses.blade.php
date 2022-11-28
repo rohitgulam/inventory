c7044980ada6eb9d315c513b302b5a5fe7eba075
rohitgulam <rohitsgulam@gmail.com>
2022-11-28 07:53:31 +0300

    :sparkles: view for all expenditures

--- resources/views/expenses.blade.php
+++ resources/views/expenses.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="pt-12">
-    <div class="w-12/12 flex flex-col items-center justify-center">
+    <div class="w-12/12 flex flex-col ">
         <div class="flex justify-between w-full my-6">
             <h2 class="text-3xl" >Orodha ya Matumizi Yote</h2>
             
@@ -14,43 +14,44 @@
                     <th class="px-6 border border-gray-400 py-2 text-center" >#</th>
                     <th class="px-6 border border-gray-400 py-2" >Jina la matumizi</th>
                     <th class="px-6 border border-gray-400 py-2">Jumla</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Siku matumizi yaliyofanyika</th> 
                     {{-- <th class="px-6 border border-gray-400 py-2" >Bei ya moja</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Bei ya jumla</th>
                     <th class="px-6 border border-gray-400 py-2">Maelezo</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa mkopo</th>
                     <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa nani</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Nani aliyefanya mauzo</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Siku mauzo yaliyofanyika</th> --}}
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Nani aliyefanya mauzo</th>--}}
                 </tr>
             </thead>
             <tbody class="text-gray-500">
-                {{-- @foreach ($expenses as $sell)
+                @foreach ($expenditure as $expense)
                     <tr>
                         <td class="pr-12 pl-4 border border-gray-400 py-2 text-center" > {{$loop->iteration}}</td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2" > {{$sell->product}}</td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$sell->quantity}} </td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2"> @money($sell->price) </td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2"> @money($sell->unit_sum) </td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2" > {{$expense->name}}</td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2"> @money($expense->amount) </td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$expense->created_at}} </td> 
+                        {{-- <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$expense->quantity}} </td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2"> @money($expense->unit_sum) </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2" > 
-                            @if ($sell->description === null)
+                            @if ($expense->description === null)
                                 Hamna maelezo
                             @else 
-                                {{$sell->description}}
+                                {{$expense->description}}
                             @endif 
                         </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2 text-center">
-                            @if ($sell->credit == 1)
+                            @if ($expense->credit == 1)
                                 Mkopo
                             @else 
                                 Sio Mkopo
                             @endif 
                         </td>
                         <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$sell->sell_to}} </td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$sell->sell_by}} </td>
-                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$sell->created_at}} </td>
+                        <td class="pr-12 pl-4 border border-gray-400 py-2"> {{$sell->sell_by}} </td>--}}
+                        
                         
                     </tr>
-                @endforeach --}}
+                @endforeach
             </tbody>
         </table>
     </div>
