e98ab83cd80701774e760616c21ecaf78ce1987f
rohitgulam <rohitsgulam@gmail.com>
2022-12-01 03:31:26 +0300

    :art: changed title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Document</title>
+    <title>Login | KG Transporters</title>
 </head>
 <body class="bg-[#E5E5E5]">
     <div class="flex flex-col justify-center items-center h-screen">
