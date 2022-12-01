e720d9424b5e7ae4c3a971e70381dc1b0f6004a1
malilojr <103340140+malilojr@users.noreply.github.com>
2022-12-02 00:37:39 +0300

    Update expenses.blade.php

--- resources/views/expenses.blade.php
+++ resources/views/expenses.blade.php
@@ -4,7 +4,7 @@
 <div class="pt-12">
     <div class="w-12/12 flex flex-col ">
         <div class="flex justify-between w-full my-6">
-            <h2 class="text-3xl" >Orodha ya Matumizi Yote</h2>
+            <h2 class="text-3xl" >Orodha ya Matumizi Yote{{__('Ependiture list')}}</h2>
             
             <a href="expense/create" class="bg-indigo-600 hover:bg-indigo-700 text-white rounded py-3 px-4" > Fanya Matumizi </a>
         </div>
@@ -12,15 +12,15 @@
             <thead>
                 <tr>
                     <th class="px-6 border border-gray-400 py-2 text-center" >#</th>
-                    <th class="px-6 border border-gray-400 py-2" >Jina la matumizi</th>
-                    <th class="px-6 border border-gray-400 py-2">Jumla</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Siku matumizi yaliyofanyika</th> 
-                    {{-- <th class="px-6 border border-gray-400 py-2" >Bei ya moja</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Bei ya jumla</th>
-                    <th class="px-6 border border-gray-400 py-2">Maelezo</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa mkopo</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa nani</th>
-                    <th class="px-6 border border-gray-400 py-2 text-center" >Nani aliyefanya mauzo</th>--}}
+                    <th class="px-6 border border-gray-400 py-2" >Jina la matumizi{{__('Expense name')}}</th>
+                    <th class="px-6 border border-gray-400 py-2">Jumla{{__('total')}}</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Siku matumizi yaliyofanyika{{__('Daily expenses')}}</th> 
+                    {{-- <th class="px-6 border border-gray-400 py-2" >Bei ya moja{{__('single expense price')}}</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Bei ya jumla{{__('total expenses')}}</th>
+                    <th class="px-6 border border-gray-400 py-2">Maelezo{{__('Description')}}</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa mkopo{{__('Sold on credit')}}</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Imeuzwa kwa nani{{__('Buyer')}}</th>
+                    <th class="px-6 border border-gray-400 py-2 text-center" >Nani aliyefanya mauzo{{__('seller')}}</th>--}}
                 </tr>
             </thead>
             <tbody class="text-gray-500">
