d64788b47ba42276ded29d9acaf74c481aff1831
rohitgulam <rohitsgulam@gmail.com>
2022-12-02 14:22:28 +0300

    added other scripts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/expense.js', 'resources/js/product.js', 'resources/js/table.js'],
             refresh: true,
         }),
     ],
