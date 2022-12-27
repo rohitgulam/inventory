ff3463b674f4350a10396c40b81a7dc7de482051
rohitgulam <rohitsgulam@gmail.com>
2022-12-27 11:53:33 +0300

    :sparkles: created soft deletes

--- app/Models/Sell.php
+++ app/Models/Sell.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sell extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'product',
